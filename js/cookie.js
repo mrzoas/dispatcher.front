12089d20e6085391b3547e9dd9d290a6a90dd016
gregory <advanced_development@inbox.ru>
2019-09-27 22:56:56 +0300

    cookies!

---
+++ js/cookie.js
@@ -0,0 +1,15 @@
+//document.cookie = "user=John";
+//document.cookie = "passwd=12312";
+
+function getCookie(name)
+{
+ 	let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+ 	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+function deleteCookie(name)
+{
+	setCookie(name, "", {'max-age': -1})
+}
+
+
+alert(getCookie("user"));
